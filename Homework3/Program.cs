8ec8169600aef0235b3030283b1eee0c26dfe92a
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-22 10:27:00 +0300

    Third task is done

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -50,6 +50,16 @@ Console.WriteLine("3D distanse is " + result);
 
 /***Задача 23**
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125*/
+3 -> 1, 8, 27               5 -> 1, 8, 27, 64, 125*/
+void Kub(double num)
+{
+    int i = 1;
+    while (i - 1 < num)
+    {
+        Console.WriteLine(i + " " + i * i * i);
+        i++;
+    }
+}
+Console.WriteLine("Please, enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Kub(num);
